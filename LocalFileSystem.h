404a441930c83940f54074ea1513d9799516f726
Emilio Monti <emilio.monti@arm.com>
2012-01-06 16:40:24 +0000

    [06 January 2012] Add a workaround to bypass the microlib file system

--- LocalFileSystem.h
+++ LocalFileSystem.h
@@ -9,6 +9,32 @@
 
 namespace mbed {
 
+FILEHANDLE local_file_open(const char* name, int flags);
+
+class LocalFileHandle : public FileHandle {
+
+public:
+    LocalFileHandle(FILEHANDLE fh);
+    
+    virtual int close();
+    
+    virtual ssize_t write(const void *buffer, size_t length);
+    
+    virtual ssize_t read(void *buffer, size_t length);
+    
+    virtual int isatty();
+    
+    virtual off_t lseek(off_t position, int whence);
+    
+    virtual int fsync();
+    
+    virtual off_t flen();
+
+protected:
+    FILEHANDLE _fh;
+    int pos;
+};
+
 /* Class: LocalFileSystem
  *  A filesystem for accessing the local mbed Microcontroller USB disk drive 
  *
