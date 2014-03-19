efef670608629d0075aae5b678e59d5e03d57540
Bogdan Marinescu <bogdan.marinescu@arm.com>
2014-03-19 18:28:32 +0000

    Release 81 of the mbed library
    Main changes:
    
    - Updates and fixes for many targets
    - LPC1768: serial interface code fixes
    - nRF51822 targets now output a .hex file
    - More exporters
    - More flexible GPIO API

--- TARGET_NUCLEO_F401RE/PeripheralNames.h
+++ TARGET_NUCLEO_F401RE/PeripheralNames.h
@@ -37,13 +37,13 @@ extern "C" {
 #endif
 
 typedef enum {
-    ADC_1 = (int)ADC1_BASE,
-    ADC_2 = (int)ADC_BASE
+    ADC_1 = (int)ADC1_BASE
 } ADCName;
 
 typedef enum {
     UART_1 = (int)USART1_BASE,  
-    UART_2 = (int)USART2_BASE
+    UART_2 = (int)USART2_BASE,
+    UART_6 = (int)USART6_BASE
 } UARTName;
 
 #define STDIO_UART_TX  PA_2
@@ -52,18 +52,25 @@ typedef enum {
 
 typedef enum {
     SPI_1 = (int)SPI1_BASE,
-    SPI_2 = (int)SPI2_BASE
+    SPI_2 = (int)SPI2_BASE,
+    SPI_3 = (int)SPI3_BASE
 } SPIName;
 
 typedef enum {
     I2C_1 = (int)I2C1_BASE,
-    I2C_2 = (int)I2C2_BASE
+    I2C_2 = (int)I2C2_BASE,
+    I2C_3 = (int)I2C3_BASE
 } I2CName;
 
 typedef enum {
-    PWM_2 = (int)TIM2_BASE,
-    PWM_3 = (int)TIM3_BASE,
-    PWM_4 = (int)TIM4_BASE
+    PWM_1  = (int)TIM1_BASE,
+    PWM_2  = (int)TIM2_BASE,
+    PWM_3  = (int)TIM3_BASE,
+    PWM_4  = (int)TIM4_BASE,
+    PWM_5  = (int)TIM5_BASE,
+    PWM_9  = (int)TIM9_BASE,
+    PWM_10 = (int)TIM10_BASE,
+    PWM_11 = (int)TIM11_BASE
 } PWMName;
 
 #ifdef __cplusplus
